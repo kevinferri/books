1558480f1d5d69ac73b1bd204e87f548ae854934
Kevin Ferri <kevinferri@gmail.com>
2015-05-14 18:17:17 -0400

    Add css and fonts in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
+var gulp = require('gulp'),
+  uglify = require('gulp-uglify'),
+  minifyCss = require('gulp-minify-css');
 
 // Uglifies js
 gulp.task('js', function() {
@@ -8,9 +9,22 @@ gulp.task('js', function() {
   .pipe(gulp.dest('build/js'));
 });
 
+// Minifies css
+gulp.task('css', function() {
+  gulp.src('./bower_components/materialize/dist/css/materialize.css')
+  .pipe(minifyCss())
+  .pipe(gulp.dest('build/css'));
+});
+
+// Pipes fonts
+gulp.task('fonts', function() {
+  gulp.src('./bower_components/materialize/dist/font/*/*')
+  .pipe(gulp.dest('build/fonts'));
+});
+
 // Watches js
 gulp.task('watch', function() {
   gulp.watch('./public/javascripts/*.js', ['scripts']);
 });
 
-gulp.task('default', ['js', 'watch']);
+gulp.task('default', ['js', 'css', 'fonts', 'watch']);
